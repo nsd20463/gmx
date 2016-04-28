a27142e771f4e9877c6481cd74723a7ee385afee
Nicolas S. Dade <nic.dade@gmail.com>
2016-04-28 16:08:24 -0700

    fix typo

--- listen.go
+++ listen.go
@@ -12,7 +12,7 @@ import (
 func localSocket() (net.Listener, error) {
 	addr := localSocketAddr()
 	os.Remove(addr.Name) // clear out any leftover use of our filename (it has our PID in it, so I feel no pity)
-	return net.ListenUnix("unit", addr)
+	return net.ListenUnix("unix", addr)
 }
 
 func localSocketAddr() *net.UnixAddr {
