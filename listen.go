ad7e6a01e592cae6484ed8bbff3812bb70a64588
Prasanna Gautam <prasannagautam@gmail.com>
2016-04-28 00:11:25 -0700

    deleting the old gmx socket file before starting new

--- listen.go
+++ listen.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+
+	"log"
 )
 
 func localSocket() (net.Listener, error) {
@@ -14,8 +16,14 @@ func localSocket() (net.Listener, error) {
 }
 
 func localSocketAddr() *net.UnixAddr {
+	gmxFilePath := filepath.Join(os.TempDir(), fmt.Sprintf(".gmx.%d.%d", os.Getpid(), GMX_VERSION))
+	if _, err := os.Stat(gmxFilePath); err == nil {
+		log.Printf("File %s already exists, deleting it before creating another\n", gmxFilePath)
+		err = os.Remove(gmxFilePath)
+		log.Printf("Unable to delete file %s. Error: %q\n", gmxFilePath, err)
+	}
 	return &net.UnixAddr{
-		filepath.Join(os.TempDir(), fmt.Sprintf(".gmx.%d.%d", os.Getpid(), GMX_VERSION)),
+		gmxFilePath,
 		"unix",
 	}
 }
